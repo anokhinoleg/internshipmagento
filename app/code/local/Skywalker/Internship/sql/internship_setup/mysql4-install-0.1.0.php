c637c21557a4b8a320311acf48d64f6a7e0a3795
Anokhin Oleg <olegyurievich1991@gmail.com>
2018-07-18 21:33:57 +0300

    create table command in installation script

--- app/code/local/Skywalker/Internship/sql/internship_setup/mysql4-install-0.1.0.php
+++ app/code/local/Skywalker/Internship/sql/internship_setup/mysql4-install-0.1.0.php
@@ -2,6 +2,14 @@
 $installer = $this;
 $installer->startSetup();
 $installer->run("
+    CREATE TABLE `{$installer->getTable('internship/contact')}` (
+      `id` int(11) NOT NULL auto_increment,
+      `name` VARCHAR(255) NOT NULL,
+      `email` VARCHAR(255) NOT NULL, 
+      PRIMARY KEY  (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+    INSERT INTO `{$installer->getTable('internship/contact')}` VALUES (1,'Oleg','olegmail.com');
     INSERT INTO `{$installer->getTable('internship/contact')}` VALUES (2,'Dima','dimamail.com');
     INSERT INTO `{$installer->getTable('internship/contact')}` VALUES (3,'Gleb','glebmail.com');
 ");
