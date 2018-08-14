cf93fca84c77ce8561adf5dfc45e33b36e47055d
Anokhin Oleg <olegyurievich1991@gmail.com>
2018-08-14 17:17:09 +0300

    adding pagination

--- app/code/local/Skywalker/InternshipBlog/Block/Post/List.php
+++ app/code/local/Skywalker/InternshipBlog/Block/Post/List.php
@@ -8,13 +8,38 @@
 
 class Skywalker_InternshipBlog_Block_Post_List extends Mage_Core_Block_Template
 {
+    public function __construct()
+    {
+        parent::__construct();
+        //set your own collection (get data from database so you can list it)
+        $collection = $this->getPosts();
+        $this->setCollection($collection);
+    }
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+
+        $pager = $this->getLayout()->createBlock('page/html_pager', 'custom.pager');
+        $pager->setAvailableLimit(array(2=>2,4=>4,8=>8,'all'=>'all'));
+        $pager->setCollection($this->getCollection());
+        $this->setChild('pager', $pager);
+        $this->getCollection()->load();
+        return $this;
+    }
+
+    //this is what you call in your .phtml file to render toolbar
+    public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
+    }
+
     public function getPosts()
     {
         $param = Mage::app()->getRequest()->getParam('category');
         $posts = Mage::getModel('internshipblog/post')->getCollection();
         $posts->addFieldToFilter('category_id', $param);
         $posts->addFieldToFilter('status', 1);
-        return $posts->getItems();
+        return $posts;
     }
 
     public function getCategory() {
